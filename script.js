b859c91a37fc47d3fe4c74ed87693ac53592f1d9
minhkstn <minhnguyenkstn@gmail.com>
2020-03-17 08:32:35 +0100

    add css and title for card

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function getNextBlack(){
+function getNextBlackCard(){
+	//scan the cards if they are black
+}
+
+function getNextWhiteCard(){
 
 }
+
+function chooseCard(){
+
+}
+function getRandomID(){
+	
+}
+// document.getElementById('wCard2').src = 'https://i.ibb.co/pQ8r6WK/demo-w-Card1.png';
 document.getElementById('nextWhiteCardButton').onclick = function(){
-	var val = document.getElementById('wCard1').value,
+	document.getElementById('wCard2').src = 'https://i.ibb.co/pQ8r6WK/demo-w-Card1.png';
 } //profile_pic.jpg
